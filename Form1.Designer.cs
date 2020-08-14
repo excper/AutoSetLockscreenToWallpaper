5b570cc9dd8ecfa151e93949b6e58d09dfefad19
Keke Tong <excper@hotmail.com>
2020-08-14 22:15:27 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.Close = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenWallpaperDir = new System.Windows.Forms.Button();
+            this.setNewWallpaperButton = new System.Windows.Forms.Button();
+            this.autoSet = new System.Windows.Forms.CheckBox();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.autoStart = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "自动设置锁屏为壁纸";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.Click += new System.EventHandler(this.notifyIcon1_Click);
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Close});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(101, 26);
+            // 
+            // Close
+            // 
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(100, 22);
+            this.Close.Text = "关闭";
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // OpenWallpaperDir
+            // 
+            this.OpenWallpaperDir.Location = new System.Drawing.Point(12, 12);
+            this.OpenWallpaperDir.Name = "OpenWallpaperDir";
+            this.OpenWallpaperDir.Size = new System.Drawing.Size(100, 23);
+            this.OpenWallpaperDir.TabIndex = 1;
+            this.OpenWallpaperDir.Text = "打开壁纸文件夹";
+            this.OpenWallpaperDir.UseVisualStyleBackColor = true;
+            this.OpenWallpaperDir.Click += new System.EventHandler(this.OpenWallpaperDir_Click);
+            // 
+            // setNewWallpaperButton
+            // 
+            this.setNewWallpaperButton.Location = new System.Drawing.Point(12, 41);
+            this.setNewWallpaperButton.Name = "setNewWallpaperButton";
+            this.setNewWallpaperButton.Size = new System.Drawing.Size(100, 23);
+            this.setNewWallpaperButton.TabIndex = 2;
+            this.setNewWallpaperButton.Text = "手动设置新壁纸";
+            this.setNewWallpaperButton.UseVisualStyleBackColor = true;
+            this.setNewWallpaperButton.Click += new System.EventHandler(this.setNewWallpaperButton_Click);
+            // 
+            // autoSet
+            // 
+            this.autoSet.AutoSize = true;
+            this.autoSet.Location = new System.Drawing.Point(12, 92);
+            this.autoSet.Name = "autoSet";
+            this.autoSet.Size = new System.Drawing.Size(204, 16);
+            this.autoSet.TabIndex = 3;
+            this.autoSet.Text = "有新的锁屏壁纸时自动设置为桌面";
+            this.autoSet.UseVisualStyleBackColor = true;
+            this.autoSet.CheckedChanged += new System.EventHandler(this.autoSetWallpaper_CheckedChanged);
+            // 
+            // autoStart
+            // 
+            this.autoStart.AutoSize = true;
+            this.autoStart.Location = new System.Drawing.Point(12, 70);
+            this.autoStart.Name = "autoStart";
+            this.autoStart.Size = new System.Drawing.Size(96, 16);
+            this.autoStart.TabIndex = 4;
+            this.autoStart.Text = "开机自动运行";
+            this.autoStart.UseVisualStyleBackColor = true;
+            this.autoStart.CheckedChanged += new System.EventHandler(this.autoStart_CheckedChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(128, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(135, 79);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseHover += new System.EventHandler(this.pictureBox1_MouseHover);
+            this.pictureBox1.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.pictureBox1_PreviewKeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(275, 126);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.autoStart);
+            this.Controls.Add(this.autoSet);
+            this.Controls.Add(this.setNewWallpaperButton);
+            this.Controls.Add(this.OpenWallpaperDir);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.helpProvider1.SetShowHelp(this, true);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "win10置锁屏为壁纸";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button OpenWallpaperDir;
+        private System.Windows.Forms.Button setNewWallpaperButton;
+        private System.Windows.Forms.CheckBox autoSet;
+        private System.Windows.Forms.HelpProvider helpProvider1;
+        private System.Windows.Forms.CheckBox autoStart;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem Close;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,479 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace WindowsFormsApp3
+{
+
+    
+    public partial class Form1 : Form
+    {
+
+        private IEnumerable<string> fileNames;
+        private IEnumerable<string> wallPapers;
+        private IEnumerator<string> enumerator;
+        private List<string> wallpapers;
+        private int index = 0;
+        private int wallpapersCount;
+       // private Image Image;
+
+        private bool sureClose = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        void MainFunction()
+        {
+            string path = OpenFileFloder();
+            if (ProcessFiles(path, out string wallpaperName))
+            {
+                Image image = Image.FromFile(wallpaperName);
+                pictureBox1.Image = image;
+                image.Dispose();
+                if (DialogResult.Yes == MessageBox.Show(this, "是否设置当前最新锁屏壁纸为桌面？","确认", MessageBoxButtons.YesNo, MessageBoxIcon.Question))
+                {
+                    SetWallpaper(wallpaperName);
+                }
+                
+                
+            }
+            
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="files"></param>
+        /// <param name="wallpaperName"></param>
+        /// <returns>True if need reset wallpaper, false if not need</returns>
+        private bool ProcessFiles(IEnumerable<string> files, out string wallpaperName)
+        {
+            if (files.Count() == 0)
+            {
+                wallpaperName = null;
+                return false;
+            }
+            bool needReset = false;
+            string dirWallpapers = @"Wallpapers";
+            FileInfo fileinfo;
+            List<DateTime> dateTimes = new List<DateTime>();
+            DateTime dateTime = DateTime.MinValue;      //记录最新文件
+            string outString = "This is a string for out!";
+
+            foreach (var file in files)
+            {
+                try
+                {
+                    fileinfo = new FileInfo(file);
+
+                    if (fileinfo.Length / 1024 < 200)
+                    {
+                        continue;   //  如果小于200kb则跳过
+                    }
+                    else
+                    {
+                        DateTime createTime = fileinfo.CreationTime;
+
+                        string createTimeString = createTime.ToString("yyyy-MM-dd-HH-mm-ss");
+                        string fileName = (createTimeString + ".jpeg");
+
+
+                        Image image;
+                        try
+                        {
+                            image = Image.FromFile(file);
+                        }
+                        catch (OutOfMemoryException)
+                        {
+
+                            continue;   //非图片格式 跳过
+                        }
+
+
+                        if (image.Width < image.Height)
+                        {
+                            /*如果是竖着的图片则跳过*/
+                            image.Dispose();
+                            continue;
+                        }
+                        else
+                        {
+                            image.Dispose();
+
+                            try
+                            {
+                                File.Copy(file, Path.Combine(dirWallpapers, fileName));
+                                //   needReset = true;       //成功拷贝新图片
+                            }
+                            catch (IOException)
+                            {
+
+                                if (new FileInfo(fileName).Length != fileinfo.Length)   //  已经有一张同名图片，但不是同一张图片
+                                {
+                                    fileName = createTimeString + "_0.jpeg";
+                                    try
+                                    {
+                                        File.Copy(file, Path.Combine(dirWallpapers, fileName));
+                                        //     needReset = true;
+                                    }
+                                    catch (IOException)
+                                    {
+                                        continue;   //改名后还是有相同的 跳过
+                                    }
+
+
+                                }
+                                else
+                                {
+                                    continue;   //同名同图 跳过
+                                }
+                            }
+                        }
+
+                        //如果这个图片日期较新 则将其保存到dateTime中
+                        if (dateTime < createTime)
+                        {
+                            dateTime = createTime;
+                            outString = fileName;
+                            needReset = true;
+                        }
+
+                    }
+
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("error"); //未处理的其它异常
+                    wallpaperName = null;
+                    return false;
+                }
+            }
+            if (!needReset)
+            {
+                wallpaperName = null;
+                return false;
+            }
+            else
+            {
+                wallpaperName = outString;
+                return true;
+            }
+        }
+        private bool ProcessFiles(string path, out string wallpaperName)
+        {
+            var files = Directory.EnumerateFiles(path);
+            return ProcessFiles(files, out wallpaperName);
+
+        }
+
+
+        private void SetWallpaper(string wallpaperName)
+        {
+            // throw new NotImplementedException();
+            [DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
+            static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+            if (!string.IsNullOrEmpty(wallpaperName) )
+            {
+
+                Image image2 = Image.FromFile(wallpaperName);
+                //Environment.GetFolderPath(Environment.SpecialFolder.)
+                string wallpaperName0 = Environment.GetEnvironmentVariable("TEMP")+@"\wallpaper.bmp";
+                image2.Save(wallpaperName0);
+                image2.Dispose();
+                SystemParametersInfo(0x14, 0, wallpaperName0, 0x2);
+
+            }
+        }
+
+        private string OpenFileFloder()
+        {
+            // throw new NotImplementedException();
+            string local = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string path = local + @"\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets";
+            return path;
+          
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //  OpenFileFloder();
+
+                var files = Directory.EnumerateFiles(OpenFileFloder());
+                var diff = files.Except(fileNames);
+                if (diff.Count() > 0)
+                {
+                    fileNames = files;
+                    if (ProcessFiles(diff, out string wallpaperName))
+                    {
+                        SetWallpaper(wallpaperName);
+                     //   timer1.Enabled = true;
+                    }
+                }
+         
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Directory.CreateDirectory(@"Wallpapers");
+            fileNames = Directory.EnumerateFiles(OpenFileFloder());
+
+            LoadXlm();
+
+            if (autoSet.Checked)
+            {
+                timer1.Enabled = true;
+            }
+            else
+            {
+                timer1.Enabled = false;
+            }
+        }
+
+        private void LoadXlm()
+        {
+            // throw new NotImplementedException();
+            XmlDocument xmlDoc = new XmlDocument();
+            try
+            {
+                xmlDoc.Load("config.xml");
+            }
+            catch (FileNotFoundException)
+            {
+                xmlDoc.Load(new StringReader("<AutoSetLockWallpaper><AutoStart></AutoStart><AutoSet></AutoSet></AutoSetLockWallpaper>"));
+                XmlDeclaration xmlDeclaration = xmlDoc.CreateXmlDeclaration("1.0", "utf-8", "yes");
+                xmlDoc.InsertBefore(xmlDeclaration, xmlDoc.DocumentElement);
+                xmlDoc.Save("config.xml");
+                return;
+            }
+
+            XmlNode xmlNode = xmlDoc.SelectSingleNode("AutoSetLockWallpaper").SelectSingleNode("AutoStart");
+            if (xmlNode.InnerText == "Yes")
+            {
+                autoStart.Checked = true;
+            }
+            else
+            {
+                autoStart.Checked = false;
+            }
+
+            xmlNode = xmlDoc.SelectSingleNode("AutoSetLockWallpaper").SelectSingleNode("AutoSet");
+            if(xmlNode.InnerText == "Yes")
+            {
+                autoSet.Checked = true;
+            }
+            else
+            {
+                autoSet.Checked = false;
+            }
+            
+
+        }
+
+        private void notifyIcon1_Click(object sender, EventArgs e)
+        {
+            /*
+*/
+        }
+
+
+
+
+        private void OpenWallpaperDir_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Explorer.exe", @"Wallpapers");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+            if (!sureClose)
+                e.Cancel = true;
+        }
+
+        private void setNewWallpaperButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Path.Combine(Application.StartupPath,"Wallpapers");
+            // openFileDialog.Filter = 
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                SetWallpaper(openFileDialog.FileName);
+            }
+
+        }
+
+        private void autoStart_CheckedChanged(object sender, EventArgs e)
+        {
+            if (autoStart.Checked)
+            {
+                try
+                {
+                    RegistryKey registry = Registry.CurrentUser.CreateSubKey(@"Software\MicroSoft\Windows\CurrentVersion\Run");
+                    registry.SetValue("AutoSetLockScreenWallpaperToDesktopWallpaper", Application.ExecutablePath);
+
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("error");
+                }
+
+            }
+            else
+            {
+                RegistryKey registry = Registry.CurrentUser.CreateSubKey(@"Software\MicroSoft\Windows\CurrentVersion\Run");
+                registry.DeleteValue("AutoSetLockScreenWallpaperToDesktopWallpaper");
+            }
+            SetXml("AutoStart", autoStart.Checked);
+        }
+
+        private void SetXml(string v, bool @checked)
+        {
+            //throw new NotImplementedException();
+            XmlDocument xmlDocument = new XmlDocument();
+            try
+            {
+                xmlDocument.Load("config.xml");
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("xml没了!!!");
+                return;
+            }
+
+            XmlNode xmlNode = xmlDocument.SelectSingleNode("AutoSetLockWallpaper").SelectSingleNode(v);
+            if (@checked)
+            {
+                xmlNode.InnerText = "Yes";
+            }
+            else
+            {
+                xmlNode.InnerText = "No";
+            }
+            xmlDocument.Save("config.xml");
+
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Visible = false;
+            }
+        }
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.Yes == MessageBox.Show(this,"关闭程序后将不能自动将锁屏设置为壁纸，确定吗？","确认", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question))
+            {
+                sureClose = true;
+                Application.Exit();
+            }
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            this.Visible = true;
+            this.WindowState = FormWindowState.Normal;
+            this.Show();
+        }
+
+        private void autoSetWallpaper_CheckedChanged(object sender, EventArgs e)
+        {
+            if (autoSet.Checked)
+            {
+                timer1.Enabled = true;
+         //       MessageBox.Show("enabled");
+            }
+            else
+            {
+                timer1.Enabled = false;
+            }
+            SetXml("AutoSet", autoSet.Checked);
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            MainFunction();
+            wallPapers = Directory.EnumerateFiles("Wallpapers");
+            wallpapers = wallPapers.ToList();
+            wallpapersCount = wallpapers.Count;
+         //   enumerator = wallPapers.GetEnumerator();
+            if (wallpapers.Count() > 0)
+            {
+                pictureBox1.Image = Image.FromFile(wallpapers[index]);
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                if (++index > wallpapersCount-1)
+                {
+                    index = 0;
+                }
+  
+            }
+            else if(e.Button == MouseButtons.Right)
+            {
+                if (--index < 0)
+                {
+                    index = wallpapersCount - 1;
+                }
+               // pictureBox1.Image = Image.FromFile(wallpapers[index]);
+            }else if(e.Button == MouseButtons.Middle)
+            {
+                SetWallpaper(wallpapers[index]);
+            }
+            Image image = Image.FromFile(wallpapers[index]);
+              Stream stream = new MemoryStream();
+              image.Save(stream, ImageFormat.Bmp);
+            image.Dispose();
+            
+            pictureBox1.Image = Image.FromStream(stream);
+              stream.Dispose();
+            stream.Close();
+           // image.Dispose();
+
+        }
+
+
+
+        private void pictureBox1_MouseHover(object sender, EventArgs e)
+        {
+            ToolTip toolTip = new ToolTip();
+            toolTip.SetToolTip(pictureBox1, "左键下一张，右键上一张，中键设置为桌面");
+        }
+
+        private void pictureBox1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyData == Keys.Space)
+            {
+                SetWallpaper(wallpapers[index]);
+            }
+        }
+    }
+
+}
